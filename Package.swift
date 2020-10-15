fbe38b68069dfd47f431a70c654fca9d0775b16c
Daniel Browne <daniel.browne@pusher.com>
2020-10-15 16:03:16 +0100

    Use string literals since enum cases aren't available with 'swift-tools-version:5.0'.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "NWWebSocket",
-    platforms: [.iOS(.v13), .macOS(.v10_15), .tvOS(.v13)],
+    platforms: [.iOS("13.0"), .macOS("10.15"), .tvOS("13.0")],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
