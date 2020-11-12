45c363ba0b30a995607679f8e7a22c20c6d13910
Paulo Andrade <paulo@outercorner.com>
2020-11-12 12:53:24 +0000

    Recreating connection instead of calling start again (it's not allowed)

--- Sources/NWWebSocket/Model/Client/NWWebSocket.swift
+++ Sources/NWWebSocket/Model/Client/NWWebSocket.swift
@@ -60,9 +60,10 @@ open class NWWebSocket: WebSocketConnection {
     // MARK: - WebSocketConnection conformance
 
     open func connect() {
-        if connection == nil {
-            connection = NWConnection(to: endpoint, using: parameters)
-        }
+        connection?.stateUpdateHandler = nil
+        connection?.cancel()
+        
+        connection = NWConnection(to: endpoint, using: parameters)
         intentionalDisconnect = false
         connection?.stateUpdateHandler = stateDidChange(to:)
         listen()
