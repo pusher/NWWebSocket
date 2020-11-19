f268a930c330fb442f2ccdda543e25e54a9303fe
Daniel Browne <daniel.browne@pusher.com>
2020-11-19 16:59:19 +0000

    Force-cancel the connection before migrating away from it

--- Sources/NWWebSocket/Model/Client/NWWebSocket.swift
+++ Sources/NWWebSocket/Model/Client/NWWebSocket.swift
@@ -217,7 +217,7 @@ open class NWWebSocket: WebSocketConnection {
 
             switch state {
             case .ready:
-                self.connection = nil
+                self.connection?.forceCancel()
                 migratedConnection.stateUpdateHandler = self.stateDidChange(to:)
                 migratedConnection.betterPathUpdateHandler = self.betterPath(isAvailable:)
                 migratedConnection.viabilityUpdateHandler = self.viabilityDidChange(isViable:)
